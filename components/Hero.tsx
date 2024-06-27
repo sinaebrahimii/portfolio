aa0151a2fd9b5bf6fe3fb5f47d2f269a2ab433be
sinaEbrahimi <sinaebrahimi31@gmail.com>
2024-06-27 22:44:14 +0330

    text generate effect component added to project

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -26,9 +27,21 @@ const Hero = () => {
           fill="pink"
         />
       </div>
-      <div className="h-screen w-full dark:bg-black-100  bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100  bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      </div>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center ">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Boobi is Goood
+          </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl "
+            words="Transformin Concepts into Seamless Experience"
+          />
+          <p className="text-sm mt-4 ">FrontEnd Developer </p>
+        </div>
       </div>
     </div>
   );
