651d42a472c959ce441d38e691119ac26ddb7b4a
sinaEbrahimi <sinaebrahimi31@gmail.com>
2024-06-28 23:42:55 +0330

    Data file created and added bento grid component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,15 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
-
+import { navItems } from "@/data";
 export default function Home() {
   return (
     <main className="relative flex justify-center items-center flex-col overflow-hidden mx-auto bg-black-100 sm:px-10 px-5">
       <div className="max-w-7xl w-full ">
-        <FloatingNav
-          navItems={[
-            { name: "Home", link: "/", icon: <FaHome /> },
-            { name: "About", link: "#about" },
-            { name: "About", link: "#about" },
-            { name: "About", link: "#about" },
-          ]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero></Hero>
+        <Grid></Grid>
       </div>
     </main>
   );
